8977849483ac5a838b2b3804719a0b70363ef9a5
lu-git-yuyu <2121833281@qq.com>
2020-07-29 17:23:35 +0800

    ljl

--- public/js/novelStartRead.js
+++ public/js/novelStartRead.js
@@ -1,11 +1,13 @@
 $(function () {
     //全局变量
-
+    let pages = []
 
     //获取节点
     let book_tittle = $(".book_tittle")
+    let header_logo = $(".header_logo")
     let book_reader_content = $(".book_reader_content")
-    let book_navigation = $(".book_navigation")
+    let mune = $(".mune")
+    // let book_navigation = $(".book_navigation")
     let tools_back = $(".book_navigation").children().eq(0)
     let tools_mnue = $(".book_navigation").children().eq(1)
     let tools_settings = $(".book_navigation").children().eq(2)
@@ -13,7 +15,7 @@ $(function () {
     let tools_888 = $(".book_navigation").children().eq(4)
     let tools_toTop = $(".book_navigation").children().eq(5)
 
-    //获取章节
+    //渲染页面
     function getChapter() {
         return new Promise(function (resolve, reject) {
             $.post("/api/book_yourChapter", {}, (res) => {
@@ -26,7 +28,7 @@ $(function () {
         })
     }
     getChapter().then(function (res) {
-        //渲染页面
+        //标题
         book_tittle.empty().append(res.book_whichChapter.page_chapter_content)
         //文章
         book_reader_content.append(
@@ -34,15 +36,29 @@ $(function () {
         )
     })
 
+    //渲染章节
+    function getPages() {
+        return new Promise(function (resolve, reject) {
+            $.post("/api/book_chapter", {}, (res) => {
+                for (let i = 0; i < res.length; i++) {
+                    pages.push(res[i].Chapter)
+                }
+                resolve();
+            })
+        })
+    }
+    getPages().then(() => {
+        pages.forEach(element => {
+            mune.append(`<li>${element}</li>`)
+        });
+    })
+
     //工具
     function tools(){
-        // $(window).scroll(function(){
-        //     console.log($(window).scrollTop(),book_navigation.offset().top)
-        //     if($(window).scrollTop()>=book_navigation.offset().top){
-        //         book_navigation.offset().top = 0
-        //         book_navigation.css("top",book_navigation.offset().top)
-        //     }
-        // })
+        //返回首页
+        header_logo.click(function(){
+            location.href = "../index.html"
+        })
 
         tools_back.click(function(){
             location.href="./novelMainPage.html"
